2e3c1d0beba9819d6c808e2e4337ea93c070382e
wangyongzhe1993 <2293045663@qq.com>
2018-02-28 19:36:29 +0800

    update

--- FuXi/src/lalala/MyConcurrentHashMap.java
+++ FuXi/src/lalala/MyConcurrentHashMap.java
@@ -7,7 +7,8 @@ import java.util.concurrent.ConcurrentHashMap;
  * 1.重要属性
  * keys value 永远不为 null.
  * node：    基本与hashMap相同但是：hash next使用volatile修饰，保证多线程下的内存可见性
- * sizeCtl：  table初始化和resize控制变量。负数时：-1表示正在被初始化。其他负数表示：-(1+正在resize的线程的数量)
+ * sizeCtl：  table初始化和resize控制变量。负数时：-1表示正在被初始化。其他负数表示：-(1+正在resize的线程的数量),正数时表示扩容阈值
+ * baseCount: table中的元素个数
  * cellsBusy：？？？
  * counterCells：？？？
  *
@@ -15,11 +16,11 @@ import java.util.concurrent.ConcurrentHashMap;
  * initTable()
  *          1.如果table未初始化进入死循环
  *          2.如果sizeCtl小于零，说明有其他线程正在进行初始化，调用yield，让出cpu，等待初始化完成
- *          3.sizeCtl大于零
+ *          3.sizeCtl大于零,原子修改sizeCtl值为-1，若成功，则进入初始化代码
  * putVal(key,value,putIfAbsent)
  *          1.检查key value都不为空
  *          2.进入死循环
- *              1)如果table未初始化，进行初始化
+ *              1)如果table未初始化，调用initTable()进行初始化
  *              2)如果已初始化，判断对应index如果为空，直接赋值(此处取值使用原子取，赋值使用cas赋值)
  *              3)如果节点hash值为-1，该节点为ForwardingNode节点(其他线程正在对其resize)，调用helpTransfer帮助resize
  *              4)以上条件都不满足，则该index存储了多个元素
@@ -29,6 +30,10 @@ import java.util.concurrent.ConcurrentHashMap;
  *                          如果hash值小于零，说明是树形结构，调用相关方法进行处理
  *                      4】判断链表长度，是否需要转换成树
  *          3.调用addCount()方法，判断是否需要resize
+ * addCount(x,check)     更新baseCount的值
+ *          为了在高并发下拥有好的性能，使用Striped64机制
+ *          1.
+ *
  * Created by wyz on 2018/2/26.
  */
 public class MyConcurrentHashMap {
@@ -39,6 +44,7 @@ public class MyConcurrentHashMap {
             @Override
             public void run() {
                 concurrentHashMap.put(1, 2);
+                concurrentHashMap.remove(1);
                 a = 2;
             }
         }.start();
---
+++ FuXi/src/lalala/MyThread.java
@@ -0,0 +1,7 @@
+package lalala;
+
+/**
+ * Created by wyz on 2018/2/28.
+ */
+public class MyThread {
+}
